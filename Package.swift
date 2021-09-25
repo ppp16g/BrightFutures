f48439d95f81121cdc6c0aea5a2e65dd0fda280a
polypropylene16g <polypropylene16g@gmail.com>
2021-09-25 19:54:03 +0900

    SPM add platforms,swiftLanguageVersions

--- Package.swift
+++ Package.swift
@@ -3,6 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "BrightFutures",
+    platforms: [
+        .macOS("10.10"),
+        .iOS("9.0")
+    ],
+    swiftLanguageVersions: [
+        5.0
+    ],
     products: [
         .library(
             name: "BrightFutures",
